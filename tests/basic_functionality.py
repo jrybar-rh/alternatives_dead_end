fafd926100a4e37ef5c8bc6f4ee441e326960d3a
Sebastian Kisela <skisela@redhat.com>
2018-04-25 16:03:11 +0200

    Add test for '--remove-all' option
    Signed-off-by: Sebastian Kisela <skisela@redhat.com>

--- tests/basic_functionality.py
+++ tests/basic_functionality.py
@@ -21,10 +21,31 @@ class Install(ConfigTest):
         """Tests whether master and slave link are linked to paths of the real files"""
         master = MasterLink(self.bin_dir)
         slave = Link(self.bin_dir, master)
-        self.assertTrue(self.alternative_install(' '.join([str(master), str(slave)])))
+        self.assertTrue(self.alternatives_install(' '.join([str(master), str(slave)])))
 
         self.assertTrue(os.path.exists(master.link))
         self.assertTrue(os.path.exists(slave.link))
 
         self.assertEqual(master.path, os.path.realpath(master.link))
         self.assertEqual(slave.path, os.path.realpath(slave.link))
+
+
+class RemoveAll(ConfigTest):
+
+    """
+    Tests removing all alternatives of a given facility
+
+    :avocado: recursive
+    """
+
+    def test_remove_all(self):
+        """Tests whether all links from master and its slaves disappeared"""
+        master = MasterLink(self.bin_dir)
+        slave = Link(self.bin_dir, master)
+        self.assertTrue(self.alternatives_install(' '.join([str(master), str(slave)])))
+        self.assertTrue(self.alternatives_remove_all(master.name))
+
+        self.assertFalse(os.path.exists(master.link))
+        self.assertFalse(os.path.exists(slave.link))
+        self.assertFalse(os.path.exists(slave.name))
+        self.assertFalse(os.path.exists(slave.name))
